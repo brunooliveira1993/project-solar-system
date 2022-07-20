7b0892818f9c3046c92ee4325e9a6dab3601fa55
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2022-07-20 14:37:11 -0300

    exercicio 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Missions from './data/missions';
+import Planets from './data/planets';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
+
+export default App;
